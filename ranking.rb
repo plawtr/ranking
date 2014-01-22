1f4d6b17304c6fae593e132076c47f4e0966fc5c
Andrey Krahmal <coolrighthere@yahoo.com>
2014-01-22 20:52:09 +0000

    Final Working reworked version

--- ranking.rb
+++ ranking.rb
@@ -1,60 +1,21 @@
-# Write a class called User that is used to calculate the amount that a user will progress through a ranking system similar to the one Codewars uses.
-
-# Business Rules:
-
-# A user starts at rank -8 and can progress all the way to 8.
-# There is no 0 (zero) rank. The next rank after -1 is 1.
-# Users will complete activities. These activities also have ranks.
-# Each time the user completes a ranked activity the users rank progress is updated based off of the activity's rank
-# The progress earned from the completed activity is relative to what the user's current rank is compared to the rank of the activity
-# A user's rank progress starts off at zero, each time the progress reaches 100 the user's rank is upgraded to the next level
-# Any remaining progress earned while in the previous rank will be applied towards the next rank's progress (we don't throw any progress away). The exception is if there is no other rank left to progress towards (Once you reach rank 8 there is no more progression).
-# A user cannot progress beyond rank 8.
-# The only acceptable range of rank values is -8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8. Any other value should raise an error.
-# The progress is scored like so:
-
-# Completing an activity that is ranked the same as that of the user's will be worth 3 points
-# Completing an activity that is ranked one ranking lower than the user's will be worth 1 point
-# Any activities completed that are ranking 2 levels or more lower than the user's ranking will be ignored
-# Completing an activity ranked higher than the current user's rank will accelerate the rank progression. The greater the difference between rankings the more the progression will be increased. The formula is 10 * d * d where d equals the difference in ranking between the activity and the user.
-# Logic Examples:
-
-# If a user ranked -8 completes an activity ranked -7 they will receive 10 progress
-# If a user ranked -8 completes an activity ranked -6 they will receive 40 progress
-# If a user ranked -8 completes an activity ranked -5 they will receive 90 progress
-# If a user ranked -8 completes an activity ranked -4 they will receive 160 progress, resulting in the user being upgraded to rank -7 and having earned 60 progress towards their next rank
-# If a user ranked -1 completes an activity ranked 1 they will receive 10 progress (remember, zero rank is ignored)
-# Code Usage Examples:
-
-# user = User.new
-# user.rank # => -8
-# user.progress # => 0
-# user.inc_progress(-7)
-# user.progress # => 10
-# user.inc_progress(-5) # will add 90 progress
-# user.progress # => 0 # progress is now zero
-# user.rank # => -7 # rank was upgraded to -7
-# Note: Codewars no longer uses this algorithm for its own ranking system. It uses a pure Math based solution that gives consistent results no matter what order a set of ranked activities are completed at.
-
-
 
 class User 
 	attr_accessor :rank, :progress
 	RANK = [-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8]
-	PROGR = [0,1,2,3,4,5,6,7,8]
 	SCORES = {0 => 3, 1 => 1}
 
 	def initialize
-		@rank, @progress = RANK[0], PROGR[0]	
+		@rank, @progress = RANK[0], 0	
 	end
 
 	def inc_progress activity_rank
 		raise "Activity Rank out of range value" unless RANK.include?(activity_rank)
+		self.progress = 0 if self.rank == 8 
 		diff = RANK.index(self.rank)-RANK.index(activity_rank)
 
-		if SCORES.include?(diff)
+		if SCORES.include?(diff) 
 			self.add_progress(SCORES[diff])
-		elsif diff < 0
+		elsif diff < 0 
 			self.add_progress(10 * diff * diff)
 		else
 			return
@@ -62,55 +23,15 @@ class User
 	end
 
 	def add_progress progress_points
-		self.progress += progress_points
+		self.progress += progress_points 
 		while self.progress >= 100 do 
-			self.progress-=100
 			add_rank
+			self.progress-=100
 		end
+		self.progress = 0 if self.rank == 8
  	end
 
 	def add_rank	 
-		self.rank = RANK[RANK.index(self.rank)+1] if RANK.index(self.rank) < 8
+		self.rank = RANK[(RANK.index(self.rank))+1] if self.rank < 8
 	end
-		
-
-
-
-	# def add_progress(number)
-	# 	self.progress+=number 
-	# 	add_rank(self.progress/100) 
-	# 	self.progress = self.progress % 100
-	# end
-
-	# def add_rank (number)
-	# 	number += 1 if number >= -self.rank
-	# 	self.rank += number
-	# 	self.rank = 8 if self.rank > 8
-	# end
-
-	# def inc_progress activity_rank
-	# 	scores = {0 => 3, -1 => 1}
-	# 	diff =  activity_rank*self.rank > 0 ? activity_rank - self.rank : activity_rank.abs - self.rank.abs + 1
-	# 	return if diff < -1 || self.rank > activity_rank
-	# 	self.add_progress(scores[diff] || diff*diff*10)
-	# end
-
-
 end
-
-# class Activity
-
-# 	attr_accessor :rank
-
-# 		def initialize rank=-8
-# 		raise "Activity Rank out of range value" unless [-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8].include?(rank)
-# 		@rank = rank
-		
-# 	end
-
-# end
-	
-
-
-
-
--- spec/ranking_spec.rb
+++ spec/ranking_spec.rb
@@ -36,69 +36,116 @@ describe User do
 		expect(user.progress).to eq(60) 
 	end
 
-# 	it "should spend progress increasing rank " do
-# 		user.add_progress(105)
-# 		expect(user.progress).to eq(5)
-# 	end
+	it "should take a user ranked -8 takes activity ranked -3 -> receives 250 progress, rank -6 and having earned 50 progress towards next rank" do
+		user.inc_progress(-3)
+		expect(user.rank).to eq(-6)
+		expect(user.progress).to eq(50) 
+	end
 
-# 	it "should not progress beyond rank 8" do
-# 		17.times{user.add_progress(101)}
-# 		expect(user.rank).to eq(8)
-# 	end
+	it "should take a user ranked -8 takes activity ranked -2 -> receives 360 progress, rank -5 and having earned 60 progress towards next rank" do
+		user.inc_progress(-2)
+		expect(user.rank).to eq(-5)
+		expect(user.progress).to eq(60) 
+	end
 
-# 	it "should skip rank zero" do
-# 		7.times{user.add_progress(100)}
-# 		user.add_progress(105)
-# 		expect(user.rank).to eq(1)
-# 	end
+	it "should take a user ranked -8 takes activity ranked 1 -> receives 640 progress, rank -2 and having earned 40 progress towards next rank" do
+		user.inc_progress(1)
+		expect(user.rank).to eq(-2)
+		expect(user.progress).to eq(40) 
+	end
 
-# 	it "should increment 3 progress if rank of user -8 and rank activity -8" do
-# 		user.inc_progress (-8)
-# 		expect(user.progress).to eq(3)
-# 	end
+	it "should take a user ranked -8 takes activity ranked 2 -> receives 810 progress, rank 1 and having earned 10 progress towards next rank" do
+		user.inc_progress(2)
+		expect(user.rank).to eq(1)
+		expect(user.progress).to eq(10) 
+	end
 
-# 	it "should increment 1 progress if rank of user -7 and rank activity -8" do
-# 		user.add_progress(100)
-# 		user.inc_progress(-8)
-# 		expect(user.progress).to eq(1)
-# 	end
+	it "should take a user ranked -8 takes activity ranked 3 -> receives 1000 progress, rank 3 and having earned 0 progress towards next rank" do
+		user.inc_progress(3)
+		expect(user.progress).to eq(0) 
+		expect(user.rank).to eq(3)
+	end
 
-# 	it "should increment 0 progress if rank of user -6 and rank activity -8" do
-# 		2.times{user.add_progress(100)}
-# 		user.inc_progress(-8)
-# 		expect(user.progress).to eq(0)
-# 	end
+	it "should take a user ranked -8 takes activity ranked 4 -> receives 1210 progress, rank 5 and having earned 10 progress towards next rank" do
+		user.inc_progress(4)
+		expect(user.progress).to eq(10) 
+		expect(user.rank).to eq(5)
+	end
 
-# 	it "should increment 90 progress if rank of user -8 and rank activity -5" do
+	it "should take a user ranked -8 takes activity ranked 5 -> receives 1440 progress, rank 7 and having earned 40 progress towards next rank" do
+		user.inc_progress(5)
+		expect(user.progress).to eq(40) 
+		expect(user.rank).to eq(7)
+	end
+
+	it "should take a user ranked -8 takes activity ranked 6 -> receives 1690 progress, rank 8 and having earned 90 progress towards next rank" do
+		user.inc_progress(6)
+		expect(user.progress).to eq(0) 
+		expect(user.rank).to eq(8)
+	end
+
+
+	it "should increment 3 progress if rank of user -8 and rank activity -8" do
+		user.inc_progress(-8)
+		expect(user.progress).to eq(3)
+	end
+
+	it "should increment 1 progress if rank of user -7 and rank activity -8" do
+		user.inc_progress(-7)
+		user.inc_progress(-5)
+		expect(user.rank).to eq(-7)
+		a = user.progress
+		user.inc_progress(-8)
+		b = user.progress
+		expect(b-a).to eq(1)
+	end
+
+	it "should increment 0 progress if rank of user -6 and rank activity -8" do
+		user.inc_progress(-3)
+		expect(user.rank).to eq(-6)
+		a = user.progress
+		user.inc_progress(-8)
+		b = user.progress
+		expect(a).to eq(b)
+	end
+
+	it "should increment 90 progress if rank of user -8 and rank activity -5" do
 		
-# 		user.inc_progress(-5)
-# 		expect(user.progress).to eq(90)
-# 	end
+		user.inc_progress(-5)
+		expect(user.progress).to eq(90)
+	end
 
-# 	it "should increment 160 progress, if rank of user -8 and rank activity -4, i.e. rank -7 and 60 progress" do
-# 		user.inc_progress(-4)
-# 		expect(user.progress).to eq(60)
-# 		expect(user.rank).to eq(-7)
-# 	end
+	it "should increment 160 progress, if rank of user -8 and rank activity -4, i.e. new rank -7 and 60 progress" do
+		user.inc_progress(-4)
+		expect(user.progress).to eq(60)
+		expect(user.rank).to eq(-7)
+	end
 
-# 	it "should check small progress raises rank" do
-# 		2.times{user.add_progress(99)}
-# 		expect(user.rank).to eq(-7)
-# 		expect(user.progress).to eq(98)
-# 	end
+	it "should check small progress raises rank" do
+		user.inc_progress(-5)
+		user.inc_progress(-5)
+		expect(user.rank).to eq(-7)
+		expect(user.progress).to eq(80)
+	end
 
-# 	it "should for user of -1 completing activity ranking +1, increments progress by 10." do
-# 		7.times{user.add_progress(100)}
-# 		user.inc_progress(1)
-# 		expect(user.progress).to eq(10)
-# 	end
+	it "should for user of -1 rank completing activity ranking +1, increment progress by 10." do
+		user.inc_progress(1)
+		expect(user.rank).to eq(-2)
+		expect(user.progress).to eq(40)
+		user.inc_progress(2) 
+		expect(user.rank).to eq(-1)
+		expect(user.progress).to eq(30)
+		user.inc_progress(1)
+		expect(user.progress).to eq(40)
+	end
 
-# 	it "should for user of 1 completing activity ranking -1, have rank of 1." do
-# 		8.times{user.add_progress(100)}
-# 		expect(user.progress).to eq(0)
-# 		user.inc_progress(-1)
-# 		expect(user.rank).to eq(1)
-# 	end
+	it "should for user of 1 completing activity ranking -1, have rank of 1." do
+		user.inc_progress(2)
+		expect(user.rank).to eq(1)
+		expect(user.progress).to eq(10) 
+		user.inc_progress(-1)
+		expect(user.rank).to eq(1)
+	end
 
 
 end
